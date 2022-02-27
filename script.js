2e6d32b507b87ccad8194d74ee5be75fd8e89ca6
Kenson Johnson <kenson.johnson@outlook.com>
2022-02-26 21:43:04 -0600

    Finished project
    Implemented a reset button that clears out the grid and rebuilds it.
    The function resetGrid() promts the user to enter a number between 8 - 100
    and uses that as the "length" of each side of the grid. So entering 24
    would create a grid filled with 24 cells by 24 cells. If anything outside
    of the given range is entered, a default 16x16 grid is created.
    
    Polished up the look of the app, adding an explaination to the page for the
    use to quickly understand what's going on.
    
    Project is now completed.

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function createGrid(size) {
     for (i = 0; i < (size * size); i++) {
         let square = document.createElement("div");
         container.appendChild(square).className = "grid-square";
+        square.style.backgroundColor = 'white';
         square.addEventListener('mouseover', changeBackground);
     };
 };
@@ -19,6 +20,31 @@ function createGrid(size) {
 //create a funtion that changes the background color of a square during mouseover event
 function changeBackground(e) {
     e.target.style.backgroundColor = 'black';
+}
+
+//create variable to add eventListener to the reset button
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', resetGrid)
+
+//impelement the resetGrid function
+function resetGrid() {
+    let size = prompt("Please enter a number between 8 - 100 to determine the size of the grid.");
+
+    //this resets all the grid squares back to white
+    let gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach(elem => {
+        elem.style.backgroundColor = 'white';
+    });
+
+    //this rebuilds the grid with the number given.
+    //numbers outside of 8 - 100 will cause the grid to default to 16x16
+    if (size >= 8 && size <= 100) {
+        createGrid(size);
+        return
+    } else {
+        createGrid(16);
+        return
+    }
 }
 
 createGrid(16);
